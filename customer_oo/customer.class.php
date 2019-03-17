8e1db117d8a117b51a6fd6cbe4de4e1128d0a6b9
gemalisk <43007423+gemalisk@users.noreply.github.com>
2019-03-17 00:02:05 -0400

    Initial Commit

---
+++ customer_oo/customer.class.php
@@ -0,0 +1,303 @@
+<?php
+if (!$_SESSION) {
+    header("Location: login.php");
+}
+
+class Customer {
+    public $id;
+    public $name;
+    public $email;
+    public $password_hash;
+    public $mobile;
+    private $noerrors = true;
+    private $nameError = null;
+    private $emailError = null;
+    private $password_hashError = null;
+    private $mobileError = null;
+    private $title = "Customer";
+    private $tableName = "customers";
+    
+    function create_record() { // display "create" form
+        $this->generate_html_top (1);
+        $this->control_group("name", $this->nameError, $this->name);
+        $this->control_group("email", $this->emailError, $this->email);
+        $this->control_group("password_hash", $this->password_hashError, $this->password_hash);
+        $this->control_group("mobile", $this->mobileError, $this->mobile);
+        $this->generate_html_bottom (1);
+    } // end function create_record()
+    
+    function read_record($id) { // display "read" form
+        $this->select_db_record($id);
+        $this->generate_html_top(2);
+        $this->control_group("name", $this->nameError, $this->name, "readonly");
+        $this->control_group("email", $this->emailError, $this->email, "readonly");
+        $this->control_group("password_hash", $this->password_hashError, $this->password_hash, "readonly");
+        $this->control_group("mobile", $this->mobileError, $this->mobile, "readonly");
+        $this->generate_html_bottom(2);
+    } // end function read_record()
+    
+    function update_record($id) { // display "update" form
+        if($this->noerrors) $this->select_db_record($id);
+        $this->generate_html_top(3, $id);
+        $this->control_group("name", $this->nameError, $this->name);
+        $this->control_group("email", $this->emailError, $this->email);
+        $this->control_group("password_hash", $this->password_hashError, $this->password_hash);
+        $this->control_group("mobile", $this->mobileError, $this->mobile);
+        $this->generate_html_bottom(3);
+    } // end function update_record()
+    
+    function delete_record($id) { // display "read" form
+        $this->select_db_record($id);
+        $this->generate_html_top(4, $id);
+        $this->control_group("name", $this->nameError, $this->name, "readonly");
+        $this->control_group("email", $this->emailError, $this->email, "readonly");
+        $this->control_group("password_hash", $this->password_hashError, $this->password_hash, "readonly");
+        $this->control_group("mobile", $this->mobileError, $this->mobile, "readonly");
+        $this->generate_html_bottom(4);
+    } // end function delete_record()
+    
+    function insert_db_record () {
+        if ($this->fieldsAllValid ()) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO $this->tableName (name,email,password_hash,mobile) values(?, ?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($this->id,$this->name,$this->email,$this->password_hash,$this->mobile));
+            Database::disconnect();
+            header("Location: $this->tableName.php");
+        }
+        else {
+            $this->create_record(); // go back to "create" form
+        }
+    } // end function insert_db_record
+    
+    private function select_db_record($id) {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM $this->tableName where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        Database::disconnect();
+        $this->name = $data['name'];
+        $this->email = $data['email'];
+        $this->password_hash = $data['password_hash'];
+        $this->mobile = $data['mobile'];
+    } // function select_db_record()
+    
+    function update_db_record ($id) {
+        $this->id = $id;
+        if ($this->fieldsAllValid()) {
+            $this->noerrors = true;
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE $this->tableName  set name = ?, email = ?, password_hash = ?, mobile = ? WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($this->name,$this->email,$this->password_hash,$this->mobile,$this->id));
+            Database::disconnect();
+            header("Location: $this->tableName.php");
+        }
+        else {
+            $this->noerrors = false;
+            $this->update_record($id);  // go back to "update" form
+        }
+    } // end function update_db_record 
+    
+    function delete_db_record($id) {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM $this->tableName WHERE id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        Database::disconnect();
+        header("Location: $this->tableName.php");
+    } // end function delete_db_record()
+    
+    private function generate_html_top ($fun, $id=null) {
+        switch ($fun) {
+            case 1: // create
+                $funWord = "Create"; $funNext = 11; 
+                break;
+            case 2: // read
+                $funWord = "Read"; $funNext = 0; 
+                break;
+            case 3: // update
+                $funWord = "Update"; $funNext = "33&id=" . $id; 
+                break;
+            case 4: // delete
+                $funWord = "Delete"; $funNext = "44&id=" . $id; 
+                break;
+            case 0: // list
+            default:
+                break;
+        }
+        echo "<!DOCTYPE html>
+        <html>
+            <head>
+                <title>$funWord a $this->title</title>
+                    ";
+        echo "
+                <meta charset='UTF-8'>
+                <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
+                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
+                    "; 
+        echo "
+            </head>";
+                echo "
+            <body>
+                <div class='container'>
+                    <div class='span10 offset1'>
+                        <p class='row'>
+                            <h3>$funWord a $this->title</h3>
+                        </p>
+                        <form class='form-horizontal' action='$this->tableName.php?fun=$funNext' method='post'>                        
+                    ";
+    } // end function generate_html_top()
+    
+    private function generate_html_bottom ($fun) {
+        switch ($fun) {
+            case 1: // create
+                $funButton = "<button type='submit' class='btn btn-success'>Create</button>"; 
+                break;
+            case 2: // read
+                $funButton = "";
+                break;
+            case 3: // update
+                $funButton = "<button type='submit' class='btn btn-warning'>Update</button>";
+                break;
+            case 4: // delete
+                $funButton = "<button type='submit' class='btn btn-danger'>Delete</button>"; 
+                break;
+            case 0: // list
+            default: // list
+                break;
+        }
+        echo " 
+                            <div class='form-actions'>
+                                $funButton
+                                <a class='btn btn-secondary' href='$this->tableName.php'>Back</a>
+                            </div>
+                        </form>
+                    </div>
+
+                </div> <!-- /container -->
+            </body>
+        </html>
+                    ";
+    } // end function generate_html_bottom()
+    
+    private function control_group ($label, $labelError, $val, $modifier="") {
+        echo "<div class='control-group";
+        echo !empty($labelError) ? ' alert alert-danger ' : '';
+        echo "'>";
+        echo "<label class='control-label'>$label</label>";
+        echo "<div class='controls'>";
+        echo "<input "
+            . "name='$label' "
+            . "type='text' "
+            . "$modifier "
+            . "placeholder='$label' "
+            . "value='";
+        echo !empty($val) ? $val : '';
+        echo "'>";
+        if (!empty($labelError)) {
+            echo "<span class='help-inline'>";
+            echo "&nbsp;&nbsp;" . $labelError;
+            echo "</span>";
+        }
+        echo "</div>";
+        echo "</div>";
+    } // end function control_group()
+    
+    private function fieldsAllValid () {
+        $valid = true;
+        if (empty($this->name)) {
+            $this->nameError = 'Please enter Name';
+            $valid = false;
+        }
+        if (empty($this->email)) {
+            $this->emailError = 'Please enter Email Address';
+            $valid = false;
+        } 
+        else if ( !filter_var($this->email,FILTER_VALIDATE_EMAIL) ) {
+            $this->emailError = 'Please enter a valid email address: me@mydomain.com';
+            $valid = false;
+        }
+        if (empty($this->password_hash)) {
+            $this->password_hashError = 'Please enter Password';
+            $valid = false;
+        } 
+        if (empty($this->mobile)) {
+            $this->mobileError = 'Please enter Mobile Number';
+            $valid = false;
+        }
+        return $valid;
+    } // end function fieldsAllValid() 
+    
+    function list_records() {
+        echo "<!DOCTYPE html>
+        <html>
+            <head>
+                <title>$this->title" . "s" . "</title>
+                    ";
+        echo "
+                <meta charset='UTF-8'>
+                <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
+                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
+                    ";  
+        echo "
+            </head>
+            <body>
+		<a href='https://github.com/gemalisk/CIS355Prog3' target='_blank'>Github</a><br />
+                <div class='container'>
+                    <p class='row'>
+                        <h3>$this->title" . "s" . "</h3>
+                    </p>
+                    <p>
+                        <a href='$this->tableName.php?fun=1' class='btn btn-success'>Create</a>
+                        <a href='logout.php'> <button>Log Out</button> </a>
+                    </p>
+                    <div class='row'>
+                        <table class='table table-striped table-bordered'>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Password</th>
+                                    <th>Mobile</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                    ";
+        $pdo = Database::connect();
+        $sql = "SELECT * FROM $this->tableName ORDER BY id";
+        foreach ($pdo->query($sql) as $row) {
+            echo "<tr>";
+            echo "<td>". $row["name"] . "</td>";
+            echo "<td>". $row["email"] . "</td>";
+            echo "<td>". $row["password_hash"] . "</td>";
+            echo "<td>". $row["mobile"] . "</td>";
+            echo "<td width=250>";
+            echo "<a class='btn btn-info' href='$this->tableName.php?fun=2&id=".$row["id"]."'>Read</a>";
+            echo "&nbsp;";
+            echo "<a class='btn btn-warning' href='$this->tableName.php?fun=3&id=".$row["id"]."'>Update</a>";
+            echo "&nbsp;";
+            echo "<a class='btn btn-danger' href='$this->tableName.php?fun=4&id=".$row["id"]."'>Delete</a>";
+            echo "</td>";
+            echo "</tr>";
+        }
+        Database::disconnect();        
+        echo "
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </body>
+
+        </html>
+                    ";  
+    } // end function list_records()
+    
+} // end class Customer
---
+++ customer_oo/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+require "../database/database.php";
+
+if($_GET) $errorMessage = $_GET['errorMessage'];
+else $errorMesage = '';
+
+if($_POST) {
+    $success = false;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT * FROM prog3 WHERE email = '$username' AND password_hash = '$password' LIMIT 1";
+    $q = $pdo->prepare($sql);
+    $q->execute(array());
+    $data = $q->fetch(PDO::FETCH_ASSOC);
+    
+    if($data) {
+        $_SESSION["username"] = $username;
+        header("Location: customer.php");
+    }
+    else {
+        header("Location: login.php?errorMessage=Invalid Credentials");
+        exit();
+    }
+}
+?>
+
+<h1>Log In</h1>
+<form class="form-horizontal" action="login.php" method="post">
+    
+    <input name="username" type="text"  placeholder="me@email.com" required>
+    <input name="password" type="password" required>
+    <button type="submit" class="btn btn-success">Sign in</button>
+    <button onclick="location.href = 'logout.php';">Log Out</button>
+    <button onclick="location.href = 'signup.php';">Sign Up</button>
+    
+    <p style='color: red;'><?php if (isset($errorMessage)) {echo $errorMessage;}?></p>
+    
+</form>
